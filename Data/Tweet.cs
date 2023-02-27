d956460576cf09348949a27bc9806f6bb61675c6
ImperialBreakCode <eeeda522@gmail.com>
2023-02-27 17:15:11 +0200

    Add project files.

---
+++ Data/Tweet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Tweet
+    {
+        public int TweetId { get; set; }
+        public string Caption { get; set; }
+        public string Image { get; set; }
+        public int Likes { get; set; }
+        public int Retweets { get; set; }
+        public int Views { get; set; }
+        public DateTime PublishDate { get; set; }
+        public User User { get; set; }
+        public List<Tweet> Replies { get; set; }
+        public Tweet ParentTweet { get; set; }
+    }
+}
