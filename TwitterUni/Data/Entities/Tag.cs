4a921ec014cb280a1bb646ab2bf4f8895df1daec
ImperialBreakCode <eeeda522@gmail.com>
2023-03-11 15:27:12 +0200

    new structure

---
+++ TwitterUni/Data/Entities/Tag.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace TwitterUni.Data.Entities
+{
+    public class Tag
+    {
+        public int TagId { get; set; }
+        public string TagName { get; set; }
+        public ICollection<Tweet> Tweets { get; set; }
+    }
+}
---
+++ TwitterUni/Data/Entities/TweetActivity.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace TwitterUni.Data.Entities
+{
+    public class TweetActivity
+    {
+        public int TweetActivityId { get; set; }
+        public string Type { get; set; }
+        public Tweet Tweet { get; set; }
+        public User Doer { get; set; }
+    }
+}
